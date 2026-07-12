1eba55cca5050cd047f4f135c07df0a555e766cb
srilekha-cmyk <bsrilekha75@gmail.com>
2026-07-12 12:36:06 +0530

    Create programing1.py

---
+++ programing1.py
@@ -0,0 +1,16 @@
+def print_numbers(n):
+    i = 1
+    while i <= n:
+        print(i)
+        i += 1
+
+n = int(input("Enter n: "))
+print_numbers(n)
+
+# Output:
+# Enter n: 5
+# 1
+# 2
+# 3
+# 4
+# 5
